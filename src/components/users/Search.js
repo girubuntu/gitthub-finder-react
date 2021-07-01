295d1f6db2195affb00a039701fc0416f6267b58
Girubuntu s <girubuntus@gmail.com>
2021-07-01 21:45:09 +0200

    second version with react

---
+++ src/components/users/Search.js
@@ -0,0 +1,42 @@
+import React, {useState, useContext} from 'react'
+import GithubContext from '../../context/github/GithubContext';
+import AlertContext from '../../context/alert/AlertContext';
+
+
+
+const Search = () => {
+
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+  const [text, setText] = useState('');
+   
+  const onChange = (e) => setText(e.target.value);
+  
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text === '') {
+       alertContext.setAlert('Please enter a text ', 'light');
+    } else {
+      githubContext.searchUsers(text);
+    setText(''); 
+    }
+    
+  }
+  
+    
+    return (
+      <div>
+        <form onSubmit={onSubmit} className="form">
+          <input type="text" name="text" placeholder="Search users...." value={text} onChange={onChange} />
+          <input type="submit" value="Search" className="btn btn-dark btn-block" />
+        </form>
+        { githubContext.users.length >0 &&
+          <button className="btn btn-light btn-block" onClick={githubContext.clearUsers}>clear</button>}
+
+        
+      </div>
+    )
+  
+}
+
+export default Search
